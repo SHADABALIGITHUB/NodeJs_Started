db315b2a48e5f7aa7781494a8d6053bab6217f57
shadab <shad46243@gmail.com>
2024-09-21 19:59:54 +0530

    Added ejs folder structure + params

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express=require('express');
-
+const path=require('path')
 const app=express();
+const env=require('dotenv').config();
+
+port=process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.join(__dirname,'..','Client')));
 
 
+app.set('view engine','ejs');
 
 app.use((req,res,next)=>{
 
@@ -12,13 +20,23 @@ app.use((req,res,next)=>{
 })
 
 app.get("/",(req,res)=>{
-
-      res.send("working i am ")
+      const MArks = 24;
+     res.render("index",{MArks})
     
+})
+
+app.get('/profile/:username',(req,res)=>{
+
+       const value=req.params.username;
+      
+
+      res.send(`<h2> My name is ${value} </h2>`)
 })
 
 
 
 
 
-app.listen(3000);
+app.listen(port,()=>{
+       console.log('Started Backend')
+});
